a6042e23b04bd4c9b905d49141781a0cc7123919
Darko Živić <zivic.darko79@gmail.com>
2025-08-11 23:14:07 +0200

    `Update SkeletonThree component: fix class names and image dimensions`

--- src/components/mainpage/introduction/SkeletonThree.tsx
+++ src/components/mainpage/introduction/SkeletonThree.tsx
@@ -10,19 +10,19 @@ export const SkeletonThree = () => {
       <a
         href="https://www.youtube.com/watch?v=k8zLN4XOUuk&t=21s"
         target="__blank"
-        className="relative flex gap-10  h-full group/image"
+        className="relative flex gap-10 h-full group/image"
       >
-        <div className="w-full  mx-auto bg-transparent dark:bg-transparent group h-full">
-          <div className="flex flex-1 w-full h-full flex-col space-y-2  relative">
+        <div className="w-full mx-auto bg-transparent group h-full">
+          <div className="flex flex-1 w-full h-full flex-col space-y-2 relative">
             {/* TODO */}
             <IconBrandYoutubeFilled className="h-20 w-20 absolute z-10 inset-0 text-red-500 m-auto " />
             <Image
               src={image}
-              alt="header"
-              width={800}
-              height={800}
+              alt="camera"
+              width={400}
+              height={400}
               priority
-              className="h-full w-full aspect-square object-cover object-center rounded-sm blur-none group-hover/image:blur-md transition-all duration-200 mb-11"
+              className="h-full w-full aspect-square object-cover object-center rounded-md blur-none group-hover/image:blur-md transition-all duration-200 mb-20"
             />
           </div>
         </div>
