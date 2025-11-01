041eab13a9957afa561fb55ffc45a6d55b8b5fe9
Darko Živić <zivic.darko79@gmail.com>
2025-11-01 10:57:09 +0100

    `Update next.config.ts with minor formatting changes and addition of turbopack configuration`

--- next.config.ts
+++ next.config.ts
@@ -2,40 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   rewrites: async () => [
-    { source: '/api/cart',        destination: 'https://wp.zivic-elektro.shop/wp-json/wc/store/v1/cart' },
-    { source: '/api/cart/add-item', destination: 'https://wp.zivic-elektro.shop/wp-json/wc/store/v1/cart/add-item' },
+    { source: "/api/cart", destination: "https://wp.zivic-elektro.shop/wp-json/wc/store/v1/cart" },
+    { source: "/api/cart/add-item", destination: "https://wp.zivic-elektro.shop/wp-json/wc/store/v1/cart/add-item" },
   ],
+
   reactStrictMode: true,
-    images: {
-        remotePatterns: [
-          {
-            protocol: "http",
-            hostname: "localhost",
-          },
-          {
-            protocol: "https",
-            hostname: "wp.zivic-elektro.shop",
-          },
-          {
-            protocol: "https",
-            hostname: "res.cloudinary.com",
-            pathname: "/dhkmlqg4o/**",
-          },
-          {
-            protocol: "https",
-            hostname: "images.unsplash.com",
-            
-          },
-          {
-            protocol: "https",
-            hostname: "assets.aceternity.com",
-          },
-          {
-            protocol: "https",
-            hostname: "api.microlink.io",
-          },
-        ],
-      },
+
+  images: {
+    remotePatterns: [
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "https", hostname: "wp.zivic-elektro.shop" },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/dhkmlqg4o/**" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "assets.aceternity.com" },
+      { protocol: "https", hostname: "api.microlink.io" },
+    ],
+  },
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
