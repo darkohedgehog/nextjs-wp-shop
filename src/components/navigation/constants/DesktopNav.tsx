5bb96fd623636e36441ed7d29f5c9105a7a6f9b6
Darko Živić <zivic.darko79@gmail.com>
2025-12-07 21:06:30 +0100

    `Update globals.css and DesktopNav.tsx with CSS changes`

--- src/components/navigation/constants/DesktopNav.tsx
+++ src/components/navigation/constants/DesktopNav.tsx
@@ -38,7 +38,7 @@ const DesktopNav = ({ navItems }: DesktopNavProps) => {
   return (
     <motion.div
       className={cn(
-        "relative z-60 mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full px-4 py-2 lg:flex bg-gradient-custom border border-cyan-800 shadow-md shadow-cyan-800",
+        "relative z-60 mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full px-4 py-2 lg:flex bg-gradient-custom border border-cyan-800 shadow-lg shadow-cyan-700",
         "sticky inset-x-0 top-40"
       )}
     >
